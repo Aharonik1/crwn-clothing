22a5992372b13c576f674c4c5ee8fb0ef0935022
Akhrorkhuja <aharonik1@mail.ru>
2020-12-09 12:44:38 +0500

    styled-components added into components and pages also removed scss files from project

---
+++ src/components/menu-item/menu-item.styles.jsx
@@ -0,0 +1,77 @@
+import styled, { css } from "styled-components";
+
+const largeMenuItemStyles = css`
+  height: 380px;
+`;
+const getMenuItemStyles = ({ size }) => {
+  if (size === "large") {
+    return largeMenuItemStyles;
+  }
+};
+
+export const MenuItemContainer = styled.div`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:first-child {
+    margin-right: 7.5px;
+  }
+
+  &:last-child {
+    margin-left: 7.5px;
+  }
+
+  ${getMenuItemStyles}
+`;
+
+export const BackgroundImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+
+  ${MenuItemContainer}:hover & {
+    transform: scale(1.1);
+    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+  }
+`;
+
+export const ContentContainer = styled.div`
+  height: 90px;
+  padding: 0 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  background-color: white;
+  opacity: 0.7;
+  position: absolute;
+
+  ${MenuItemContainer}:hover & {
+    opacity: 0.9;
+  }
+
+  .title {
+    font-weight: bold;
+    margin-bottom: 6px;
+    font-size: 22px;
+    color: #4a4a4a;
+  }
+
+  .subtitle {
+    font-weight: lighter;
+    font-size: 16px;
+  }
+`;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import FormInput from "../form-input/form-input.component";
 
 import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
-import "./sign-in.styles.scss";
+import { SignInContainer, ButtonsContainer } from "./sign-in.styles";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -38,8 +38,8 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="sign-in">
-        <h2>I have already an account</h2>
+      <SignInContainer>
+        <h2 className="title">I have already an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -59,14 +59,14 @@ class SignIn extends React.Component {
             label="Password"
             required
           />
-          <div className="buttons">
+          <ButtonsContainer>
             <CustomButton type="submit">Sign in</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
-          </div>
+          </ButtonsContainer>
         </form>
-      </div>
+      </SignInContainer>
     );
   }
 }
